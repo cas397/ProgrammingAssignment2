c81a81806575d95fb732fa271cc06c818acdb851
cas397 <cas397@cornell.edu>
2015-11-11 00:47:52 -0500

    R Programming Assignment 2
    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix will create a list containing 4 functions
+## The 4 functions will set the matrix, get the matrix
+## set the inverse, and get the inverse
+## cacheSolve will obtain the inverse of the matrix from the
+## cache if its there, otherwise it will solve for it and
+## store the inverse in the cache
 
-## Write a short comment describing this function
+## The input z is the matrix that you want to find the
+## inverse of. makeCacheMatrix will return a list containing the
+## functions mentioned above, as well as the input matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(z) {
+  I <- NULL
+  set <- function(y) {
+    x <<- y
+    I <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(Inverse) I <<- Inverse
+  getInverse <- function() I
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse, Mat = z)
 }
 
 
-## Write a short comment describing this function
+## The input is a variable containing the list returned 
+## from makeCacheMatrix
+## This will return the inverse of the matrix entered
+## into makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Returns the inverse of the matrix contained in x from the cache
+  I <- x$getInverse()
+  
+  ## Determines if the inverse is stored in the cache & returns
+  ## the inverse if it is
+  if(!is.null(I)) {
+    message("getting cached data")
+    return(I)
+  }
+  
+  ## Set the matrix to be the matrix contained in list x & find
+  ## and find the inverse and store it in the cache
+  Matrix <- x$set(x$Mat)
+  data <- x$get()
+  I <- solve(data, ...)
+  x$setInverse(I)
+  
+  ##Return the Inverse Matrix
+  I
 }
